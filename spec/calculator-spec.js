e1a03e18bbce492adc8f22a6867d8c67a0508a34
John Murray <JMurray.Seattle@gmail.com>
2018-08-08 15:28:31 -0700

    Passing Test for converting age to seconds (ageInYears and birthday)

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,14 +1,34 @@
-import { Calculator } from '../src/age-calculator.js';
+import { AgeCalculator } from '../src/age-calculator.js';
 
-describe('Calculator', function() {
-  var reusableCalculator;
+describe('AgeCalculator', function() {
+  var reusableAgeCalculator;
 
   beforeEach(function() {
-    reusableCalculator = new Calculator((2013, 1, 1), 95);
+    reusableAgeCalculator = new AgeCalculator(25, "1993,3,3", 135);
   });
   
-  it('should test that the Calculator constructor works', function() {
-    expect(reusableCalculator.birthday).toEqual((2013, 1, 1));
-    expect(reusableCalculator.lifeExpectancy).toEqual(95);
+  it('should test that the AgeCalculator constructor works and accepts propper property types', function() {
+    expect(reusableAgeCalculator.ageInYears).toEqual(25);
+    expect(reusableAgeCalculator.ageInYears).not.toEqual("");
+    expect(reusableAgeCalculator.birthday).toEqual("1993,3,3");
+    expect(reusableAgeCalculator.birthday).not.toEqual(1993,3,3);
+    expect(reusableAgeCalculator.lifeExpectancy).toEqual(135);
+    expect(reusableAgeCalculator.lifeExpectancy).not.toEqual("135");
+  });
+
+  it('should convert a persons age in years into seconds', function() {
+    expect(reusableAgeCalculator.yearsToSeconds()).not.toBe("");
+    expect(reusableAgeCalculator.yearsToSeconds()).toEqual(788400000);
+  });
+
+  it('should convert a persons birthday into age in seconds', function() {
+    expect(reusableAgeCalculator.ageInSeconds()).not.toBe("");
+  });
+
+  it('should determine the difference in seconds between two dates', function() {
+    let date1 = new Date(1999,1,1);
+    let date2 = new Date(1999,1,2);
+    let difference = dateDifference(date1, date2);
+    expect(difference).not.toBe("");
   });
 });
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,7 +1,16 @@
-export class Calculator {
-  constructor(birthday = new Date(), lifeExpectancy) {
-
+export class AgeCalculator {
+  constructor(ageInYears, birthday, lifeExpectancy) {
+    this.ageInYears = ageInYears;
     this.birthday = birthday;
     this.lifeExpectancy = lifeExpectancy;
+  }
+
+  yearsToSeconds() {
+    return this.ageInYears * 31536000;
+  }
+
+  ageInSeconds() {
+    let date = new Date(this.birthday);
+    date.getTime() / 1000;
   }
 }
