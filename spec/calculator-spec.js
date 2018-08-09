2767ace5d1b230f51b1583a27aa7003f1a43ac17
John Murray <JMurray.Seattle@gmail.com>
2018-08-09 10:21:25 -0700

    red green refactor for age on mercury

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,24 +1,24 @@
-import { AgeCalculator, dateDifference } from '../src/age-calculator.js';
+import { AgeCalculator, dateDifferenceInSeconds } from '../src/age-calculator.js';
 
 describe('AgeCalculator', function() {
   var reusableAgeCalculator;
 
   beforeEach(function() {
-    reusableAgeCalculator = new AgeCalculator(25, "1993,2,3", 135);
+    reusableAgeCalculator = new AgeCalculator(25, "March 3, 1993", 135);
   });
   
   it('should test that the AgeCalculator constructor works and accepts propper property types', function() {
     expect(reusableAgeCalculator.ageInYears).toEqual(25);
     expect(reusableAgeCalculator.ageInYears).not.toEqual("");
-    expect(reusableAgeCalculator.birthday).toEqual("1993,2,3");
-    expect(reusableAgeCalculator.birthday).not.toEqual(1993,2,3);
+    expect(reusableAgeCalculator.birthday).toEqual("March 3, 1993");
+    expect(reusableAgeCalculator.birthday).not.toEqual(1993);
     expect(reusableAgeCalculator.lifeExpectancy).toEqual(135);
     expect(reusableAgeCalculator.lifeExpectancy).not.toEqual("135");
   });
 
   it('should convert a persons age in years into seconds', function() {
-    expect(reusableAgeCalculator.yearsToSeconds()).not.toBe("");
-    expect(reusableAgeCalculator.yearsToSeconds()).toEqual(788400000);
+    expect(reusableAgeCalculator.ageInYearsToSeconds()).not.toBe("");
+    expect(reusableAgeCalculator.ageInYearsToSeconds()).toEqual(788400000);
   });
 
   it('should convert a persons birthday into age in seconds', function() {
@@ -29,8 +29,18 @@ describe('AgeCalculator', function() {
   it('should determine the difference in seconds between two dates', function() {
     let date1 = new Date(1999,1,1);
     let date2 = new Date(1999,1,2);
-    let difference = dateDifference(date1, date2);
+    let difference = dateDifferenceInSeconds(date1, date2);
     expect(difference).not.toBe("");
     expect(difference).toBeCloseTo(86400);
+  });
+
+  it('should return the users age on Mercury', function(){
+    expect(reusableAgeCalculator.ageOnMercury()).not.toBe("");
+    expect(reusableAgeCalculator.ageOnMercury()).toBeCloseTo(104.167);
+  });
+
+  it('should return the users age on Venus', function(){
+    expect(reusableAgeCalculator.ageOnVenus()).not.toBe("");
+    expect(reusableAgeCalculator.ageOnVenus()).toBeCloseTo(40.323);
   });
 });
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,22 +1,36 @@
 export class AgeCalculator {
-  constructor(ageInYears, birthday, lifeExpectancy) {
+  constructor(ageInYears, birthday = new Date(), lifeExpectancy) {
     this.ageInYears = ageInYears;
     this.birthday = birthday;
     this.lifeExpectancy = lifeExpectancy;
   }
 
-  yearsToSeconds() {
+  ageInYearsToSeconds() {
     return this.ageInYears * 31536000;
   }
 
   ageInSeconds() {
-    let date = new Date(this.birthday);
-    return date.getTime() / 1000;
+    let birthday = new Date(this.birthday);
+    return birthday.getTime() / 1000;
+  }
+
+  ageOnMercury() {
+    return getAge(this.birthday) / .24;
   }
 }
 
-export function dateDifference(date1, date2) 
-{
-  var difference =(date1.getTime() - date2.getTime()) / 1000;
+export function dateDifferenceInSeconds(date1, date2) {
+  let difference =(date1.getTime() - date2.getTime()) / 1000;
   return Math.abs(Math.round(difference));
+}
+
+export function getAge(dateString) {
+  let today = new Date();
+  let birthDate = new Date(dateString);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  let m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
 }
